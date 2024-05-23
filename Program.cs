f0e2f5ea570a1afb9d1c9986394c055dacfc1495
Iva Apostolova <137160725+Ivaap@users.noreply.github.com>
2024-05-23 09:00:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2_Change_List
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            string comands;
+            while ((comands = Console.ReadLine()) != "end")
+            {
+                string[] argument = comands.Split();
+                int index;
+                switch (argument[0])
+                {
+                    case "Insert":
+                        int item = int.Parse(argument[1]);
+                        index = int.Parse(argument[2]);
+                        list.Insert(index, item);
+                        break;
+                    case "Delete":
+                        item = int.Parse(argument[1]);
+                        list.Remove(item);
+                        break;
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
