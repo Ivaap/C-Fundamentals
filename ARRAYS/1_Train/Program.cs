1e752c88e3ad91fc2b7e4b9a508a44891729b172
Iva Apostolova <137160725+Ivaap@users.noreply.github.com>
2024-05-25 13:14:46 +0300

    Add files via upload

---
+++ ARRAYS/1_Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1_Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int countWagon = int.Parse(Console.ReadLine());
+            int[] arrayTrain = new int[countWagon];
+            int sum = 0;
+            for(int i =0; i < countWagon; i++)
+            {
+                int countPeople= int.Parse(Console.ReadLine());
+                arrayTrain[i] = countPeople;
+                sum += countPeople;
+            }
+            Console.WriteLine(string.Join(" ",arrayTrain));
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ ARRAYS/4_Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace _4_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            PrintTriangle(count);
+
+        }
+        static void PrintTriangle(int count)
+        {
+            for( int col = 1;  col<=count; col++)
+            {
+                for(int row =1;row<=col; row++)
+                {
+                    Console.Write($"{row} ");
+                }
+                Console.WriteLine();
+            }
+            for(int i =1; i <count; i--)
+            {
+                for (int j =1; j <=i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
