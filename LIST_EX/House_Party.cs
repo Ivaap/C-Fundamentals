857d6ba7226afa1b79a5a21c8165e792aae4c752
Iva Apostolova <137160725+Ivaap@users.noreply.github.com>
2024-06-17 12:33:42 +0300

    Add files via upload

---
+++ LIST_EX/House_Party.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3_House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countGuest = int.Parse(Console.ReadLine());
+            List<string> guestList   = new List<string>();
+            for (int i = 0; i < countGuest; i++)
+            {
+                string[] arguments = Console.ReadLine().Split();
+                string name = arguments[0];
+                if (arguments[2] == "going!")
+                {
+                    AddGuest(guestList, name);
+                }
+                else if (arguments[2] == "not")
+                {
+                    RemoveGuest(guestList, name);
+                }
+            }
+
+            Console.WriteLine(string.Join("n", guestList));
+        }
+
+        private static void AddGuest(List<string> guestList, string name)
+        {
+            if (guestList.FindIndex(x => x == name) == -1)
+            {
+                guestList.Add(name);
+            }
+            else
+            {
+                Console.WriteLine($"{name} is already in the list!");
+            }
+        }
+
+        private static void RemoveGuest(List<string> guestList, string name)
+        {
+            if (guestList.FindIndex(x => x == name) == -1)
+            {
+                Console.WriteLine($"{name} is not in the list!");
+            }
+            else
+            {
+                guestList.Remove(name);
+            }
+        }
+    }
+}
+
---
+++ LIST_EX/List_Operation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4_List_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int>numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            string comand;
+            while ((comand = Console.ReadLine()) != "End")
+            {
+                string[] argument = comand.Split(" ");
+                switch (argument[0])
+                {
+                    case "Add":
+                        numbers.Add(int.Parse(argument[1]));
+                        break;
+                    case "Insert":
+                        int item = int.Parse(argument[1]);
+                        int insertIndex = int.Parse(argument[2]);
+                        if (IsNotValidIndex(insertIndex, numbers.Count))
+                        {
+                            Console.WriteLine(" Invalid index");
+                            continue;
+                        }
+                        numbers.Insert(insertIndex, item);
+                        break;
+                    case "Remove":
+                        int removeIndex = int.Parse(argument[1]);
+                        if (IsNotValidIndex(removeIndex, numbers.Count))
+                        {
+                            Console.WriteLine("Invalid index");
+                            continue;
+                        }
+                        numbers.Remove(removeIndex);
+                        break;
+                    case "Shift":
+                        string direction = argument[1];
+                        int count = int.Parse(argument[2]);
+                        Shift(numbers, direction, count);
+                        break;
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+            
+        }
+        private static bool IsNotValidIndex(int index, int count)
+        {
+            return index < 0 || index >= count;
+        }
+        private static void Shift (List<int>numbers, string direction, int count)
+        {
+            count %= numbers.Count;
+            if(direction == "left")
+            {
+                List<int> shiftedPart = numbers.GetRange(0, count);
+                numbers.RemoveRange(0, count);
+                numbers.InsertRange(numbers.Count, shiftedPart);
+            }
+            else if(direction == "right")
+            {
+                List<int> shiftedPart = numbers.GetRange(numbers.Count - count, count);
+                numbers.RemoveRange(numbers.Count - count, count);
+                numbers.InsertRange(0, shiftedPart);
+            }
+        }
+
+    }
+}
